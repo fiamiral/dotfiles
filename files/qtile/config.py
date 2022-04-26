e5dd86bc0ec19b0035936bdf800b9653db8efba8
fiamiral <fiamiral@lumilize.me>
2022-04-27 01:16:42 +0900

    ✨ feat[qtile]: enhanced initial config

--- files/qtile/config.py
+++ files/qtile/config.py
@@ -1,5 +1,5 @@
 from libqtile import bar, layout, widget
-from libqtile.config import Click, Drag, Group, Key, Match, Screen
+from libqtile.config import Click, Drag, Group, Key, Match, Screen, ScratchPad, DropDown
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
 
@@ -9,18 +9,27 @@ mod = "mod4"
 
 # Keybinds
 keys = [
-    # System
+    # Qtile
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Exit Qtile"),
+    Key([mod], "Return", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
 
     # Window
-    Key([mod], "q", lazy.window.kill(), desc="Kill current window"),
+    Key([mod, "shift"], "q", lazy.window.kill(), desc="Kill current window"),
 
-    # temp
-    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+    # Launch
+    Key([mod], "s", lazy.spawn("alacritty"), desc="Launch alacritty"),
+
+    # ScratchPad
+    Key([mod], "i", lazy.group["scratchpad"].dropdown_toggle("term1")),
 ]
 
 groups = [
+    ScratchPad("scratchpad", [
+        DropDown("term1", "alacritty",
+            width = 0.5, height = 0.5,
+            x = 0.25, y = 0.25),
+    ]),
     Group("1"),
     Group("2"),
     Group("3"),
@@ -28,21 +37,65 @@ groups = [
 ]
 
 layouts = [
-    layout.Columns(),
+    layout.Columns(
+        border_focus = "#D08770",
+        border_normal = "#8FBCBB",
+        border_on_single = True,
+        margin = 8,
+    ),
 ]
 
-
+#--------------------------------
+# Screens
+#--------------------------------
 screens = [
     Screen(
         bottom=bar.Bar([
-            widget.GroupBox(),
-            widget.Prompt(),
-            widget.WindowName(),
-            widget.Systray(),
-            widget.Clock(format="%Y-%m-%d %H:%M:%S"),
-        ], 30), # 30 is Height of bar
+                widget.GroupBox(
+                    this_current_screen_border = "#D08770",
+                    this_screen_border = "#D08770",
+                    other_current_screen_border = "#88C0D0",
+                    other_screen_border = "#88C0D0",
+                    inactive = "#4C566A",
+                ),
+                widget.Spacer(),
+                widget.WindowName(),
+                widget.Prompt(),
+                widget.Volume(fmt="🔊: {}"),
+                widget.Sep(),
+                widget.Clock(format="%Y-%m-%d %H:%M:%S"),
+            ], size = 30,
+            background = "#2E3440",
+            opacity = 0.8,),
+        wallpaper = '~/sandbox/wallpaper.jpg',
+        wallpaper_mode = "fill",
     ),
-    Screen(),
+    Screen(
+        bottom=bar.Bar([
+                widget.GroupBox(
+                    this_current_screen_border = "#D08770",
+                    this_screen_border = "#D08770",
+                    other_current_screen_border = "#88C0D0",
+                    other_screen_border = "#88C0D0",
+                    inactive = "#4C566A",
+                ),
+                widget.Spacer(),
+                widget.WindowName(),
+                widget.Prompt(),
+                widget.Volume(fmt="🔊: {}"),
+                widget.Sep(),
+                widget.Clock(format="%Y-%m-%d %H:%M:%S"),
+            ], size = 30,
+            background = "#2E3440",
+            opacity = 0.8,),
+        wallpaper = '~/sandbox/wallpaper.jpg',
+        wallpaper_mode = "fill",
+    ),
 ]
 
-
+widget_defaults = dict(
+    font='Cica',
+    fontsize=16,
+    padding=3,
+    foreground="#D8DEE9"
+)
