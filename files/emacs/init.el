d4eae6f059fb6eaf196f4ee80aa47d8d8850aadb
fiamiral <fiamiral@lumilize.me>
2022-06-05 23:15:53 +0900

    🐎 perf[emacs]: don't ask 'Symbolic link to Git-~'

--- files/emacs/init.el
+++ files/emacs/init.el
@@ -1,5 +1,6 @@
-(require 'org)
+(setq vc-follow-symlinks t)
 
+(require 'org)
 (org-babel-tangle-file (expand-file-name "init-org.org" user-emacs-directory)
                        (expand-file-name "init-org.el" user-emacs-directory))
 (load (expand-file-name "init-org.el" user-emacs-directory))
