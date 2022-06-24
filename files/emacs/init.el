6cf7cb9e90c2efd0d0b9dca801e96d764cad473d
fiamiral <fiamiral@lumilize.me>
2022-06-24 15:03:41 +0900

    ✨ feat[emacs]: auto compile init-org.org

--- files/emacs/init.el
+++ files/emacs/init.el
@@ -1,6 +1,9 @@
 (setq vc-follow-symlinks t)
 
-(require 'org)
-(org-babel-tangle-file (expand-file-name "init-org.org" user-emacs-directory)
-                       (expand-file-name "init-org.el" user-emacs-directory))
-(load (expand-file-name "init-org.el" user-emacs-directory))
+(when (file-newer-than-file-p (expand-file-name "init-org.org" user-emacs-directory)
+                              (expand-file-name "init-org.elc" user-emacs-directory))
+  (require 'org)
+  (org-babel-tangle-file (expand-file-name "init-org.org" user-emacs-directory)
+                         (expand-file-name "init-org.el" user-emacs-directory))
+  (byte-compile-file (expand-file-name "init-org.el" user-emacs-directory)))
+(load (expand-file-name "init-org.elc" user-emacs-directory))
