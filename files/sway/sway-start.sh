85b1378cbc80f8051742925eed97a30354001714
fiamiral <fiamiral@lumilize.me>
2022-06-28 18:54:02 +0900

    🐎 perf[sway]: add env for fcitx5 to sway-start

--- files/sway/sway-start.sh
+++ files/sway/sway-start.sh
@@ -6,5 +6,10 @@ export WINIT_UNIX_BACKEND=x11
 # firefox
 export MOZ_ENABLE_WAYLAND=1
 
+#fcitx5
+export GTK_IM_MODULE=fcitx5
+export QT_IM_MODULE=fcitx5
+export XMODIFIERS=@im=fcitx5
+
 sleep 1
 exec sway
