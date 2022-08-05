7ba98abaa5a256082681b4e083bd67da9ce928e1
fiamiral <fiamiral@lumilize.me>
2022-08-06 00:06:53 +0900

    ✨ feat[emacs]: set user-emacs-directory for debug

--- files/emacs/init.el
+++ files/emacs/init.el
@@ -1,3 +1,7 @@
+; set user-emacs-directory to the folder where located this file
+(when load-file-name
+  (setq user-emacs-directory (file-name-directory load-file-name)))
+
 (setq vc-follow-symlinks t)
 
 (when (file-newer-than-file-p (expand-file-name "init-org.org" user-emacs-directory)
