099b80642227937e68cf4383aadb1fa837c3affd
fiamiral <fiamiral@lumilize.me>
2022-08-06 04:23:46 +0900

    🐎 perf[emacs]: stop GC on startup

--- files/emacs/init.el
+++ files/emacs/init.el
@@ -2,6 +2,9 @@
 (when load-file-name
   (setq user-emacs-directory (file-name-directory load-file-name)))
 
+;stop GC on startup
+(setq gc-cons-threshold most-positive-fixnum)
+
 (setq vc-follow-symlinks t)
 
 (when (file-newer-than-file-p (expand-file-name "init-org.org" user-emacs-directory)
@@ -11,3 +14,6 @@
                          (expand-file-name "init-org.el" user-emacs-directory))
   (byte-compile-file (expand-file-name "init-org.el" user-emacs-directory)))
 (load (expand-file-name "init-org.elc" user-emacs-directory))
+
+;resume GC
+(setq gc-cons-threshold 16777216)
