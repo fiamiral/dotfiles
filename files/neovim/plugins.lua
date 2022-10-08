26380281f9341f31a68768cd75f936d2a0c0c881
fiamiral <fiamiral@lumilize.me>
2022-10-08 22:59:48 +0900

    ✨ feat[neovim]: like IDE

--- files/neovim/plugins.lua
+++ files/neovim/plugins.lua
@@ -20,9 +20,25 @@ local packer_bootstrap = ensure_packer()
 
 return require("packer").startup(function(use)
     use("wbthomason/packer.nvim")
-    -- My plugins here
+    -- Nord colorscheme
     use("arcticicestudio/nord-vim")
-
+    -- LSP
+    use({ "williamboman/mason.nvim", "neovim/nvim-lspconfig", "williamboman/mason-lspconfig.nvim" })
+    -- debug
+    use({ "nvim-lua/plenary.nvim", "mfussenegger/nvim-dap" })
+    -- completion
+    use({
+        "hrsh7th/nvim-cmp",
+        "hrsh7th/cmp-nvim-lsp",
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-cmdline",
+        "saadparwaiz1/cmp_luasnip",
+    })
+    -- rust
+    use({"simrat39/rust-tools.nvim", 'rust-lang/rust.vim'})
+    -- snippet
+    use({ "L3MON4D3/LuaSnip", tag = "v<CurrentMajor>.*" })
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if packer_bootstrap then
