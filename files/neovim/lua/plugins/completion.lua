98f5cf1ee9c72945019d957d99b79d1b58af38e9
fiamiral <fiamiral@lumilize.me>
2023-04-06 03:31:28 +0900

    feat(neovim): change completion keybind

--- files/neovim/lua/plugins/completion.lua
+++ files/neovim/lua/plugins/completion.lua
@@ -27,11 +27,7 @@ return {
                 mapping = {
                     ["<Tab>"] = cmp.mapping(function(fallback)
                         if cmp.visible() then
-                            if not cmp.get_selected_entry() then
-                                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-                            else
-                                cmp.confirm()
-                            end
+                            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
                         else
                             fallback()
                         end
